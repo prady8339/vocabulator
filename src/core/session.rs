c4cd91240c8edad820a54e2890167b22e9ffbd92
AnshUjlayan <anshujlayan3@gmail.com>
2026-02-16 07:00:40 +0530

    expand session struct

--- src/core/session.rs
+++ src/core/session.rs
@@ -1,7 +1,29 @@
 use crate::db::models::Word;
 
-#[derive(Debug, Clone)]
 pub struct Session {
     pub words: Vec<Word>,
     pub index: usize,
+
+    // UI state
+    pub show_definition: bool,
+    pub graded: Option<bool>,
+    pub input_buffer: String,
+    pub insert_mode: bool,
+}
+
+impl Session {
+    pub fn current(&self) -> &Word {
+        &self.words[self.index]
+    }
+
+    pub fn current_mut(&mut self) -> &mut Word {
+        &mut self.words[self.index]
+    }
+
+    pub fn reset_ui_state(&mut self) {
+        self.show_definition = false;
+        self.graded = None;
+        self.input_buffer.clear();
+        self.insert_mode = false;
+    }
 }
